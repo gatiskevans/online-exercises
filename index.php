815bcc0e3c3582cd4a54f8c7e91d35ce0df622b9
Gatis Ķevans <gatis.kevans@gmail.com>
2021-10-01 20:36:14 +0300

    empty

--- index.php
+++ index.php
@@ -1,24 +1,3 @@
 <?php
 
-interface Example
-{
-    public function getName(): string;
-}
-
-class ExampleTwo implements Example
-{
-    private string $name;
-
-    public function __construct(string $name)
-    {
-        $this->name = $name;
-    }
-
-    public function getName(): string
-    {
-        return $this->name;
-    }
-}
-
-$example = new ExampleTwo("John Doe");
-echo $example->getName();
+    
